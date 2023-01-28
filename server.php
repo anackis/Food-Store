faa63ee33dc5c2502946e13d84e18d706683c621
Alex <rengarsanj@gmail.com>
2023-01-28 19:51:40 +0200

    small change

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php 
 $_POST = json_decode(file_get_contents("php://input"), true);
-// Pozvaljajet poluchitj json dannije i porabotatj s nimi 
+/// Alow you to get json files and work with them 
 echo var_dump($_POST);
-// prevrashjaet prishedshije dannije v stroku i pokazivaet na kliente 
+/// Translate data that we got to string and show on client 
